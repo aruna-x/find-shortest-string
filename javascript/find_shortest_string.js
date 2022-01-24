201d5fb0f25945c15f8ca981fc42a083ddd18d50
Aruna <aruna.evan15@gmail.com>
2022-01-24 14:08:50 -0800

    Solution

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,9 +1,22 @@
 function findShortestString(arr) {
-  // type your code here
+  let res = arr[0];
+  for(let i=0; i<arr.length; i++){
+    res = res.length > arr[i].length ? arr[i] : res;
+    // if (res.length > arr[i].length){
+    //   res = arr[i];
+    // }
+  }
+  return res;
 }
 
+/**
+ * Alternatively, can just store index num and return arr[num].
+ * Makes no difference for space or time complexity though.
+ */
+
+
 if (require.main === module) {
-  // add your own tests in here
+
   console.log("Expecting: 'a'");
   console.log("=>", findShortestString(['aaa', 'a', 'bb', 'ccc']));
 
@@ -17,10 +30,33 @@ if (require.main === module) {
   console.log("Expecting: 'lily'");
   console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
 
+  console.log("");
+
+  console.log("Expecting: 'solo'");
+  console.log("=>", findShortestString(['solo']));
+
   // BENCHMARK HERE
 }
 
 module.exports = findShortestString;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+/**
+ * 1. paraphrase
+ * 
+ * Array of strings (min length 1), return first shortest string.
+ * 
+ * 2. tests
+ * 
+ * ['a']
+ * 
+ * 3. pseudocode solution
+ * 
+ * var to hold string res takes the first one automatically
+ * for each successive string from array, compares length
+ * if shorter, replace with shorter. Else move on to next string to compare.
+ * 
+ * 4. code (above)
+ * 5. refactor (above)
+ * 6. optimize
+ * 
+ */
